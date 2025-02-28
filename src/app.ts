818888c75478580d10b50ada54ed8332cf5c35e6
Raí <devvandenberg>
2025-02-27 21:34:06 -0300

    criando rota e controllers

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import "express-async-errors"
 
+import { routes } from './routes';
+
 import { errorHandling } from './middlewares/erro-handling';
 
 const app = express()
 
 app.use(express.json())
+app.use(routes)
 
 app.use(errorHandling)
 
--- src/middlewares/erro-handling.ts
+++ src/middlewares/erro-handling.ts
@@ -1,10 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "@/utils/AppError";
+import { ZodError } from "zod";
+
+
 export function errorHandling( error: any, request: Request, response: Response, next: NextFunction){
 
 if(error instanceof AppError){
 
     return response.status(error.statusCode).json({ message: error.message})
+
+}
+
+if(error instanceof ZodError){
+
+    response.status(400).json({message: "validation error", issues: error.format(),})
 
 }
 
