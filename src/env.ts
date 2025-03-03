285bbe907534e67e139c5c8c9f8208cf074e10e0
Raí <devvandenberg>
2025-03-03 19:33:54 -0300

    test de cadatramento de usuario

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,11 @@
 import { z } from "zod";
+import dotenv from "dotenv";
+
+dotenv.config(); // Carrega as variáveis do .env
 
 const envSchema = z.object({
     DATABASE_URL: z.string().url(),
     JWT_SECRET: z.string()
-})
+});
 
-export const env = envSchema.parse(process.env)
+export const env = envSchema.parse(process.env);
