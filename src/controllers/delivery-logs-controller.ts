f9c5e87675dc80acf3914b366f9d67c7a7ad71bd
Raí <devvandenberg>
2025-03-05 22:08:38 -0300

    fix

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -51,6 +51,10 @@ class DeliveryLogsConstroller{
             }
         })
 
+        if(!delivery){
+            return response.status(404).json({ message: "delivery not found"})
+        }
+
         if(request.user?.role === "customer" && request.user.id !== delivery?.userId){
             throw new AppError("the user can only view thier deliveries", 401)
         }
